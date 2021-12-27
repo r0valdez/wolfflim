5b44d2e1a73ff026806c5a45bedaf0930c0edc6c
r0valdez <rodolfovaldez.tech@gmail.com>
2021-12-27 14:49:11 +0100

    correcties

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,29 +1,29 @@
 <template>
   <footer
-    class="flex justify-center xl:justify-between items-center flex-wrap gap-y-2 gap-x-3 md:gap-x-4 p-4 md:p-6 xl:px-10"
+    class="flex justify-center items-center flex-wrap gap-y-2 gap-x-3 md:gap-x-4 p-4 md:p-6 xl:px-10"
   >
-    <img src="~/assets/images/logo-bnn-vara.png" alt="logo bnn-vara" />
-    <img
-      src="~/assets/images/logo-natuurmonumenten.png"
-      alt="logo natuurmonumenten"
-    />
-    <img src="~/assets/images/logo-ifaw.png" alt="logo ifaw" />
-    <img src="~/assets/images/logo-iwp.png" alt="logo iwp" />
-    <img
-      src="~/assets/images/logo-gelderland.png"
-      alt="logo provincie gelderland"
-    />
-    <img src="~/assets/images/logo-veluweop1.png" alt="logo veluweop1" />
-    <img src="~/assets/images/logo-ark.png" alt="logo ark" />
+    <!--    <img src="~/assets/images/logo-bnn-vara.png" alt="logo bnn-vara" />-->
+    <!--    <img-->
+    <!--      src="~/assets/images/logo-natuurmonumenten.png"-->
+    <!--      alt="logo natuurmonumenten"-->
+    <!--    />-->
+    <!--    <img src="~/assets/images/logo-ifaw.png" alt="logo ifaw" />-->
+    <!--    <img src="~/assets/images/logo-iwp.png" alt="logo iwp" />-->
+    <!--    <img-->
+    <!--      src="~/assets/images/logo-gelderland.png"-->
+    <!--      alt="logo provincie gelderland"-->
+    <!--    />-->
+    <!--    <img src="~/assets/images/logo-veluweop1.png" alt="logo veluweop1" />-->
+    <!--    <img src="~/assets/images/logo-ark.png" alt="logo ark" />-->
+    <!--    <img-->
+    <!--        src="~/assets/images/logo-zoogdier.png"-->
+    <!--        alt="logoZoogdier vereniging"-->
+    <!--    />-->
+    <!--    <img-->
+    <!--        src="~/assets/images/logo-rewilding-europe.png"-->
+    <!--        alt="logo Rewilding Europe"-->
+    <!--    />-->
     <img src="~/assets/images/logo-M-N-Film.png" alt="logo M&N Media Group" />
-    <img
-      src="~/assets/images/logo-zoogdier.png"
-      alt="logoZoogdier vereniging"
-    />
-    <img
-      src="~/assets/images/logo-rewilding-europe.png"
-      alt="logo Rewilding Europe"
-    />
   </footer>
 </template>
 
